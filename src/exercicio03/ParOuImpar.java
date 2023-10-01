5a424fa34c4926d8d1405ac59e39586e26991bce
Camila <camiart@gmail.com>
2023-10-01 10:59:06 -0300

    Add existing file

--- src/exercicio03/ParOuImpar.java
+++ src/exercicio03/ParOuImpar.java
@@ -1,2 +1,24 @@
-package exercicio03;public class ParOuImpar {
+package exercicio03;
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número");
+        String numString = scanner.nextLine();
+
+        int numero = Integer.parseInt(numString);
+
+        if  (numero %2 == 0){
+            System.out.println("O número que você digitou é par!");
+        } else {
+            System.out.println("O número que você digitou é impar!");
+        }
+
+
+
+    }
 }
