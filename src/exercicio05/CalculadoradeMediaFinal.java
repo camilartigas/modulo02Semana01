11ab40000b9b18432c004b0906981099cf5f156e
Camila <camiart@gmail.com>
2023-10-09 19:59:26 -0300

    finalizado ex05

--- src/exercicio05/CalculadoradeMediaFinal.java
+++ src/exercicio05/CalculadoradeMediaFinal.java
@@ -1,2 +1,22 @@
-package exercicio05;public class CalculadoradeMediaFinal {
+package exercicio05;
+
+import java.util.Scanner;
+
+public class CalculadoradeMediaFinal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+        System.out.printf("A média das notas é: %.2f%n", media);
+        scanner.close();
+    }
 }
